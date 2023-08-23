911e98985837f56b14ef24eb085e327f72154f90
fabionasc <fabio.nascimentobrsp@gmail.com>
2023-08-23 01:03:47 -0300

    Teste de linha duplicada

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -20,6 +20,7 @@ namespace WinFormsApp1
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Text = "TESTE";
+            this.Text = "TESTE";
         }
     }
 }
